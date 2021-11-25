ce69ce4926c8696ffceca60428541608d38044a7
Lucille Huang <lucillejyhuang@gmail.com>
2021-11-24 21:16:50 -0500

    Section3 Colour Segmentation

--- Lab5/Lab5.m
+++ Lab5/Lab5.m
@@ -82,31 +82,61 @@ figure;
 imshow(peppers_recombined_2, []);
 title('Recombined 2');
 
-%% 3 Colour Segmentation
-f = imread('peppers.png');
+%% 3 Colour Segmentation - R2021a cannot run this section, only R2021b works. R2021a gives error: Undefined function 'kmeans' for input arguments of type 'double'.
+f = imread("peppers.png");
 % Create a color transformation structure that defines an sRGB to L*a*b* conversion.
 C = makecform('srgb2lab');
 % Perform the transformation by using the applycform function.
 im_lab = applycform(f,C);
 
-% First reshape the a* and b* channels:
-ab = double(im_lab(:,:,2:3)); % NOT im2double
+% First reshape the a∗ and b∗ channels:
+ab = double(im_lab(:,:,2:3));  % NOT im2double
 m = size(ab,1);
 n = size(ab,2);
 ab = reshape(ab,m*n,2);
 
 % K = 2;
-% row = [55 200];
-% col = [155 400];
+% row = [55   200];
+% col = [155   400];
 K = 4;
-row = [55 130 200 280];
-col = [155 110 400 470];
-
+row = [55    130   200  280];
+col = [155   110   400  470];
 % Convert (r,c) indexing to 1D linear indexing.
 idx = sub2ind([size(f,1) size(f,2)], row, col);
 % Vectorize starting coordinates
 for k = 1:K
-    mu(k,:) = ab(idx(k),:);
+  mu(k,:) = ab(idx(k),:);
 end
 
-idx = kmeans(ab, K, 'Start', mu)
+cluster_idx = kmeans(ab, K, 'Start', mu);
+
+% Label each pixel according to k-means
+pixel_labels = reshape(cluster_idx, m, n);
+h = figure,imshow(pixel_labels, [])
+title('Image labeled by cluster index, K=2');
+colormap('jet')
+
+% output = repmat(pixel_labels,4);
+% mask1 = pixel_labels==1;
+% cluster1 = f .* uint8(mask1);
+% figure
+% imshow(cluster1);
+% title('Objects in Cluster 1');
+
+% mask2 = pixel_labels==2;
+% cluster2 = f .* uint8(mask2);
+% figure
+% imshow(cluster2);
+% title('Objects in Cluster 2');
+
+% mask3 = pixel_labels==3;
+% cluster3 = f .* uint8(mask3);
+% figure
+% imshow(cluster3);
+% title('Objects in Cluster 3');
+
+% mask4 = pixel_labels==4;
+% cluster4 = f .* uint8(mask4);
+% figure
+% imshow(cluster4);
+% title('Objects in Cluster 4');

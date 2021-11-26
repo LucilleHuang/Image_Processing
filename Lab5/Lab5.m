f3cf464aed8e112a3ed5a6f0f52fa709e928bfcb
Lucille Huang <lucillejyhuang@gmail.com>
2021-11-26 11:34:58 -0500

    4 Image Transfer and 5 Quantization

--- Lab5/Lab5.m
+++ Lab5/Lab5.m
@@ -5,42 +5,42 @@ close all;
 %% 2 Chroma Subsampling use grey or colourful?
 
 %% split to Y, Cb and Cr channels - grey
-peppers = imread('peppers.png');
-peppers = rgb2ycbcr(peppers);
-[yImage, crImage, cbImage] = imsplit(peppers);
+RGB = imread('peppers.png');
+YCBCR = rgb2ycbcr(RGB);
+[Y, Cb, Cr] = imsplit(YCBCR);
 
 % Display the original & the various different Y, Cb and Cr
 figure;
-imshow(yImage, []);
+imshow(Y, []);
 title('Y Image', 'FontSize', 16);
 figure;
-imshow(cbImage, []);
+imshow(Cb, []);
 title('Cb Image', 'FontSize', 16);
 figure;
-imshow(crImage, []);
+imshow(Cr, []);
 title('Cr Image', 'FontSize', 16);
 
-%% split to Y, Cb and Cr channels - colourful like the slides
-RGB = imread('peppers.png');
-YCBCR = rgb2ycbcr(RGB);
-
-figure;
-
-lb={'Y','Cb','Cr'};
-for channel=1:3
-    subplot(1,3,channel)
-    YCBCR_C=YCBCR;
-    YCBCR_C(:,:,setdiff(1:3,channel))=intmax(class(YCBCR_C))/2;
-    imshow(ycbcr2rgb(YCBCR_C))
-    title([lb{channel} ' component'],'FontSize',18);
-    if channel == 1
-        Y = YCBCR_C;
-    elseif channel == 2
-        Cb = YCBCR_C;
-    else
-        Cr = YCBCR_C;
-    end
-end
+% %% split to Y, Cb and Cr channels - colourful like the slides
+% RGB = imread('peppers.png');
+% YCBCR = rgb2ycbcr(RGB);
+% 
+% figure;
+% 
+% lb={'Y','Cb','Cr'};
+% for channel=1:3
+%     subplot(1,3,channel)
+%     YCBCR_C=YCBCR;
+%     YCBCR_C(:,:,setdiff(1:3,channel))=intmax(class(YCBCR_C))/2;
+%     imshow(ycbcr2rgb(YCBCR_C))
+%     title([lb{channel} ' component'],'FontSize',18);
+%     if channel == 1
+%         Y = YCBCR_C;
+%     elseif channel == 2
+%         Cb = YCBCR_C;
+%     else
+%         Cr = YCBCR_C;
+%     end
+% end
 
 %% Chroma subsampling
 cbImage_reduced = imresize(Cb, 1/2);
@@ -63,23 +63,24 @@ crImage_upsamp = imresize(crImage_reduced, 2);
 % imshow(crImage_upsamp, []);
 % title('Cr Image2', 'FontSize', 16);
 
-peppers_recombined = cat(3, Y, Cb, Cr);
+recombined = cat(3, Y, Cb, Cr);
+recombined_RGB = ycbcr2rgb(recombined);
 
 figure;
-imshow(YCBCR, []);
+imshow(RGB, []);
 title('Original');
 
 figure;
-imshow(peppers_recombined);
+imshow(recombined_RGB);
 title('Recombined');
 
 %% Luma sub-sampling
-yImage_reduced = imresize(yImage, 1/2);
+yImage_reduced = imresize(Y, 1/2);
 yImage_upsamp = imresize(yImage_reduced, 2);
 
-peppers_recombined_2 = cat(3, yImage_upsamp, cbImage, crImage);
+recombined_2 = cat(3, yImage_upsamp, Cb, Cr);
 figure;
-imshow(peppers_recombined_2, []);
+imshow(recombined_2, []);
 title('Recombined 2');
 
 %% 3 Colour Segmentation - R2021a cannot run this section, only R2021b works. R2021a gives error: Undefined function 'kmeans' for input arguments of type 'double'.
@@ -140,3 +141,62 @@ colormap('jet')
 % figure
 % imshow(cluster4);
 % title('Objects in Cluster 4');
+
+%% 4 Image Transform
+T = dctmtx(8);
+figure
+imshow(T); title('DCT transform matrix');
+lena = imread('lena.tiff');
+f = double(rgb2gray(lena));
+
+DCT = floor(blkproc(f-128,[8 8],'P1*x*P2',T,T'));
+figure
+imshow(abs(DCT),[]); title('F-trans');
+figure
+imshow(abs(DCT(81:88,297:304)), []); title('DCT sub-img at (81,297)');
+figure
+imshow(abs(DCT(1:8,1:8)), []); title('DCT sub-img at (1,1)');
+
+mask = zeros(8,8);
+mask ( 1 , 1 ) = 1 ;
+mask ( 1 , 2 ) = 1 ;
+mask ( 1 , 3 ) = 1 ;
+mask ( 2 , 1 ) = 1 ;
+mask ( 3 , 1 ) = 1 ;
+mask ( 2 , 2 ) = 1 ;
+F_thresh = blkproc(DCT, [8 8], 'P1.*x', mask);
+f_thresh = floor(blkproc(F_thresh, [8 8], 'P1*x*P2',T',T))+128;
+
+figure
+imshow(f_thresh, []); title('reconstruct 3');
+reconstruct_psnr = psnr(f_thresh,f);
+
+figure
+imshow(f,[]); title('original');
+
+%% 5 Quatization
+T = dctmtx(8);
+lena = imread('lena.tiff');
+f = double(rgb2gray(lena));
+
+DCT = floor(blkproc(f-128,[8 8],'P1*x*P2',T,T'));
+
+Z = [16 11 10 16 24 40 51 61;
+    12 12 14 19 26 58 60 55;
+    14 13 16 24 40 57 69 56;
+    14 17 22 29 51 87 80 62;
+    18 22 37 56 68 109 103 77;
+    24 35 55 64 81 104 113 92;
+    49 64 78 87 103 121 120 101;
+    72 92 95 98 112 100 103 99];
+
+P1 = [1,3,5,10];
+for i = 1:size(P1,2)
+    quantized_DCT = round(blkproc(DCT, [8 8], 'P1/x', P1(i)*Z));
+    unquantized_DCT = round(blkproc(quantized_DCT, [8 8], 'P1*x', P1(i)*Z));
+    reconstruct_Z = floor(blkproc(unquantized_DCT, [8 8], 'P1*x*P2',T',T))+128;
+
+    figure
+    imshow(reconstruct_Z,[]); title(sprintf('reconstruct %dZ', P1(i)));
+    reconstruct_psnr = psnr(reconstruct_Z,f);
+end
